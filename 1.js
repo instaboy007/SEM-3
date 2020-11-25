e18760bc009d58f85236e770ec3a985130543d3a
instaboy007 <72603656+instaboy007@users.noreply.github.com>
2020-11-25 14:00:44 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,16 +1,16 @@
-sequalize.query('CREATE  TRIGGER PercentageCalc BEFORE INSERT ON attendances'+ 
+const row=sequalize.query('CREATE  TRIGGER PercentageCalc BEFORE INSERT ON attendances'+ 
 'FOR EACH ROW'+
 'SET NEW.Percentage =(NEW.PeriodNo/NEW.TotalPeriodNo)*100;')
 
-sequalize.query('CREATE TRIGGER findavg BEFORE INSERT ON TMarks'+
+const row=sequalize.query('CREATE TRIGGER findavg BEFORE INSERT ON TMarks'+
 'FOR EACH ROW'+
 'SET NEW.CA =(NEW.CA1_Mark/2 + NEW.CA2_Mark/2);')
 
-sequalize.query('CREATE TRIGGER findtotal BEFORE INSERT ON TMarks'+
+const row=sequalize.query('CREATE TRIGGER findtotal BEFORE INSERT ON TMarks'+
 'FOR EACH ROW'+
 'SET NEW.Total =(NEW.CA+ NEW.Assignment_Mark + NEW.Tutorial_Mark);')
 
-sequalize.query('CREATE TRIGGER findltotal BEFORE INSERT ON LMarks'+
+const row=sequalize.query('CREATE TRIGGER findltotal BEFORE INSERT ON LMarks'+
 'FOR EACH ROW'+
 'SET NEW.Total =(NEW.Lab1_Mark + NEW.Lab2_Mark + NEW.LabFinal_Mark + NEW.Package_Mark);')
 
@@ -20,3 +20,7 @@ BEGIN
 SELECT StudentId,TRUNCATE(SUM(credit*Gradepoint)/SUM(credit),2) AS cgpa,Result FORM semresults WHERE StudentId=studentid;
 END $$
 delimiter ;
+
+const row = await sequelize.query('call cgpa1();').spread(
+    function (req.params.id) {
+});
